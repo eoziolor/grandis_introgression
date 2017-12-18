e57e31337647d10853689d231b202daeaa680d5e
Elias Oziolor <oziolor@gmail.com>
2017-12-18 14:30:19 -0800

    created random set of regions to create basal mds plot for random genome section. converted gene ids to killifish gene ids.

--- scripts/dfst/outlier_ann.sh
+++ scripts/dfst/outlier_ann.sh
@@ -2,8 +2,8 @@
 
 my_bed=/home/elias/program/bedtools2/bin/bedtools
 my_gff=/home/elias/analysis/data/genome/unsplit_sorted.gff
-my_snps=/home/elias/analysis/data/dfst/outliers/zshared_out_all
-my_out=/home/elias/analysis/data/dfst/outliers/zshared_all_ann
+my_snps=/home/elias/analysis/data/dfst/zpbs_regions_sharedall_formatted
+my_out=/home/elias/analysis/data/dfst/outliers/pbs_regions_shared_ann
 
 $my_bed intersect \
 -a $my_gff \
--- scripts/dfst/subsample_regions.r
+++ scripts/dfst/subsample_regions.r
@@ -1,16 +1,32 @@
-source("http://bioconductor.org/biocLite.R")
-biocLite()
-library("rtracklayer")
-
-bed1=import("~/analysis/data/dfst/PBS_keep_5kb.bed")
-bedall=import("~/analysis/data/dfst/pbs_regions_sharedall.bed")
-bed1overlall=bed1[bed1 %over% bedall]
-hitsall<-findOverlaps(bedall,bed1)
-allhit<-subjectHits(hitsall)
-regions<-queryHits((hitsall))
-
 orig<-read.table("~/analysis/data/dfst/zregions_split5kb.bed",header=F)
 
+v<-c()
+for(i in 1:69){
+  mini<-sample(x=which(regions==i),size=7)
+  v<-c(v,mini)
+}
+v<-sort(v)
+
+write.table(orig[v,],"~/analysis/data/dfst/zshared_10perregion.bed",row.names = FALSE,col.names = FALSE,quote = FALSE)
+
+###Doing the same for random regions
+orig<-read.table("~/analysis/data/dfst/random/5kb1kb.bed",header=F)
+v<-sample(x=length(orig[,1]),size=483)
+v<-sort(v)
+write.table(orig[v,],"~/analysis/data/dfst/random/random_483win.bed",row.names = FALSE,col.names = FALSE,quote = FALSE)
+
+
+#tried it other ways
+# source("http://bioconductor.org/biocLite.R")
+# biocLite()
+# library("rtracklayer")
+
+# bed1=import("~/analysis/data/dfst/PBS_keep_5kb.bed")
+# bedall=import("~/analysis/data/dfst/pbs_regions_sharedall.bed")
+# bed1overlall=bed1[bed1 %over% bedall]
+# hitsall<-findOverlaps(bedall,bed1)
+# allhit<-subjectHits(hitsall)
+# regions<-queryHits((hitsall))
 # p<-c()
 # for(i in 1:69){
 #   p[i]<-sum(regions==i)/length(regions)
@@ -21,12 +37,3 @@ orig<-read.table("~/analysis/data/dfst/zregions_split5kb.bed",header=F)
 #   pp<-c(pp,rep(p[i],sum(regions==i)))
 # }
 # pp<-unlist(pp)
-
-v<-c()
-for(i in 1:69){
-  mini<-sample(x=which(regions==i),size=7)
-  v<-c(v,mini)
-}
-v<-sort(v)
-
-write.table(orig[v,],"~/analysis/data/dfst/zshared_10perregion.bed",row.names = FALSE,col.names = FALSE,quote = FALSE)
