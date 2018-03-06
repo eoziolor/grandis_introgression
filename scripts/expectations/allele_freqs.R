1b72a412f504591542f2b9e79956c973e39ae972
Elias Oziolor <oziolor@gmail.com>
2018-03-05 18:31:17 -0800

    more tinkering with allele frequencies

--- scripts/expectations/allele_freqs.R
+++ scripts/expectations/allele_freqs.R
@@ -7,22 +7,30 @@ library(stringr)
 library(gtools)
 library(rtracklayer)
 library(ggplot2)
-library(cluster)
+
 ###Finding overlaps for the bigger regions if we want to----
+pbs_dist<-read.table("~/analysis/data/dfst/zpbs_5kb.bed",header=FALSE)
+distnames<-c("Scaf","start","end","bb","vb","pb","sj","bnp")
+colnames(pbs_dist)<-distnames
+col<-c()
+for (i in 1:5){
+  col[i]<-quantile(pbs_dist[,i+3],prob=.99,na.rm=TRUE)
+}
 
-pbs_out<-read.table("~/analysis/data/dfst/zregions_max.bed",stringsAsFactors = FALSE) #loads a pbs vector with windows merged within 50kb of each other and with max and windows count statistics
+###Overall outliers
+z<-read.table("~/analysis/data/expectations/zregions_max.bed",stringsAsFactors = FALSE) #loads a pbs vector with windows merged within 50kb of each other and with max and windows count statistics
 names<-c("Scaf","start","end","BBmax","BBcount","VBmax","VBcount","PBmax","PBcount","SJmax","SJcount","BNPmax","BNPcount")
-colnames(pbs_out)<-names
+colnames(z)<-names
 
-all<-pbs_out[,4]>col[1] & pbs_out[,6]>col[2] & pbs_out[,8]>col[3] & pbs_out[,10]>col[4] & pbs_out[,12]>col[5]
+all<-z[,4]>col[1] & z[,6]>col[2] & z[,8]>col[3] & z[,10]>col[4] & z[,12]>col[5]
 
-write.table(pbs_out[all,1:3],"~/analysis/data/expectations/zall_max.bed",row.names=FALSE,col.names = FALSE,quote = FALSE)
+write.table(na.omit(z[all,1:3]),"~/analysis/data/expectations/zall_max.bed",row.names=FALSE,col.names = FALSE,quote = FALSE)
 ###Take this into kodiak to make a vcf file of the outlier regions----
 
 #Plug that VCF in the following
 
 ###Reading in VCF, formatting ----
-vcf<-read.table("~/analysis/data/dfst/outliers/zshared_haplo.vcf.bgz",stringsAsFactors = FALSE) #vcf that has been filtered out to only present one allele call per site per individual
+vcf<-read.table("~/analysis/data/expectations/zall_max_haplo.vcf.bgz",stringsAsFactors = FALSE) #vcf that has been filtered out to only present one allele call per site per individual
 sexscore<-read.table("~/analysis/scripts/depth/sexscore",header=TRUE)
 #vcf<-cbind(vcf[,1],vcf[,2]-1,vcf[,2:297]) #Converting the first three columns into bed type format
 cname<-c(seq(1:9),as.character(sexscore[,1])) # colnames for the vcf
@@ -60,9 +68,9 @@ int<-(rowSums(popfr[,6:7])/2)
 ref<-(rowSums(popfr[,8:9])/2)
 rr<-cbind(popfr[,1:2],res,ref,int) #merging these into a single DF
 
-quantile(with(rr,res-ref),probs=0.9675)
-plot(rr[,"res"]-rr[,"ref"],pch=20,cex=.4,col=ifelse(res-ref>.184,"red","black")) #Plotting difference between res and ref
-subw<-with(rr,res-ref)>.2
+quantile(with(rr,res-ref),probs=0.97)
+plot(rr[,"res"]-rr[,"ref"],pch=20,cex=.4,col=ifelse(res-ref>.13,"red","black")) #Plotting difference between res and ref
+subw<-with(rr,res-ref)>.13
 
 plot(rr[subw,"res"]-rr[subw,"int"],pch=20,cex=.4,col="firebrick2")
 points(rr[subw,"res"]-rr[subw,"ref"],pch=20,cex=.4,col="cadetblue3")
@@ -74,4 +82,49 @@ points(rr[subw,"res"]-rr[subw,"ref"],pch=20,cex=.4,col="cadetblue3")
 #        aes((res-int)/(res-ref)))+
 #   geom_histogram()
 
-                 
+###Ranking individuals based on those SNPs----
+
+vcfo<-vcf[subw,]
+zn<-na.omit(z[all,1:3])
+zn2<-matrix(nrow = length(zn[,1]),ncol=288)
+zn<-cbind(zn,zn2)
+nam<-c("chr","start","end",colnames(vcf[,10:297]))
+colnames(zn)<-nam
+
+vcfo<-cbind(vcfo[,1],vcfo[,2]-1,vcfo[,2:297])
+write.table(vcfo[,1:3],"~/analysis/data/expectations/vcf_out.bed",row.names = FALSE,col.names = FALSE,quote = FALSE)
+write.table(zn[,1:3],"~/analysis/data/expectations/zshared_all.bed",row.names = FALSE,col.names = FALSE,quote = FALSE)
+
+bed1=import("~/analysis/data/expectations/vcf_out.bed")
+bedall=import("~/analysis/data/expectations/zshared_all.bed")
+bed1overlall=bed1[bed1 %over% bedall]
+hitsall<-countOverlaps(bedall,bed1)
+subset<-subsetByOverlaps(bedall,bed1)
+
+gt<-as.matrix(vcfo[,10:297]) #convert to numeric
+class(gt)<-"numeric"
+keep<-colMeans(is.na(gt))<0.9 #toss sites with low coverage
+gt<-gt[,keep]
+
+fr<-c()
+j=0
+for(i in 1:length(hitsall)){
+  if(hitsall[i]>1){
+  f<-as.numeric(colSums(gt[(j+1):(hitsall[i]+j),],na.rm=TRUE)/hitsall[i])
+  fr<-rbind(fr,f)
+  j=j+hitsall[i]
+  print(j==sum(hitsall[1:i]))
+  print(i)
+}
+}
+
+# for(i in 1:length(zn[,1])){
+#   j=1
+#   while(zn[i,1]==vcfo[j,1]){
+#     if(zn[i,2]<=vcfo[j,2] && zn[i,3]>=vcfo[j,2])
+#     zn[i,4:291]<-zn[i,4:291]+vcfo[j,10:297]
+#     j=j+1
+#   }
+# }
+
+as.numeric(colSums(gt[sum(hitsall[1:3]):sum(hitsall[1:4]),],na.rm=TRUE)/hitsall[4])
