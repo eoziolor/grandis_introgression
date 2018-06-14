6e7efa3a64bd8da6573d354e7f2cd97089385642
Elias Oziolor <oziolor@gmail.com>
2018-06-13 17:37:19 -0700

    working on getting the figures together for the paper

--- scripts/depth/pullgenos.r
+++ scripts/depth/pullgenos.r
@@ -1,11 +1,11 @@
-pullgenos<-function(scaf,vcf="~/analysis/data/depth/chr1b.vcf.gz"){
+pullgenos<-function(scaf,vcf="~/analysis/data/depth/chr1_short.vcf.bgz"){
 
 	cline<-paste("tabix ", vcf, " ", scaf,sep="")
 	header<-scan(pipe(paste("zcat",vcf, "| grep -v '##' | head -n 1",sep=" ")),what="character")
 	gts<-read.table(pipe(cline),stringsAsFactors=FALSE)
 
 	colnames(gts)<-header
-	gts2<-as.matrix(gts[,10:297])
+	gts2<-as.matrix(gts[,10:585])
 	gts2<-gsub(":.*","",gts2)
 	gts2[gts2=="."]<-NA
 	gts2[gts2=="0/0"]<-0
