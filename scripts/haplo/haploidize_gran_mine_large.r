c8f0ad645a66905ff07f02f6104160e1079300b8
Elias Oziolor <oziolor@gmail.com>
2018-06-28 15:15:36 -0700

    downloaded introgressed region from chromosome 10 and started haploidizing it, different calling so I modified the haploidize_gran function

---
+++ scripts/haplo/haploidize_gran_mine_large.r
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+
+library(stringr)
+library(magrittr)
+
+sam<-function(x){
+      y <- str_split(x,":")[[1]][1] %>% 
+    			 unlist() %>% 
+    			 as.numeric() 
+        return(y)
+      }
+
+f <- file("stdin")
+#f<-file("~/analysis/data/dfst/outliers/zshared.vcf.bgz")
+open(f)
+while(length(line <- readLines(f,n=1)) > 0) {
+  if(grepl("^#",line)){write(line,stdout());next()}
+
+  line <- str_split(line,"\\t") %>% unlist()
+  line[10:585] <- sapply(line[10:585],sam)
+  line <- paste(line,collapse="\t")
+  write(line,stdout())
+  # write(line, stderr())
+  # process line
+}
+
+
+
